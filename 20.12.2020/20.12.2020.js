6408b551a57f01330ee6ba6881d4af2428381731
zahidmughal <zahisohna@gmail.com>
2020-12-21 15:08:42 -0800

    done today

--- 20.12.2020/20.12.2020.js
+++ 20.12.2020/20.12.2020.js
@@ -1,15 +1,31 @@
-var a ; 
-var a ;
-var a ;
-a = 6;
-console.log(a);
+// var a ; 
+// var a ;
+// var a ;
+// a = 6;
+// console.log(a);
 
-let b;
-b = 8;
-b=12;
-console.log(b);
+// let b;
+// b = 8;
+// b=12;
+// console.log(b);
 
-const c=10;
-c=30;
+// const c=10;
+// c=30;
 
-console.log(c);
+// console.log(c);
+console.log("If you have senior citizen and have MBBS, you got Visa")
+const age = 66;
+let eligible = false;
+let s_citizen = false;
+let y_citizen =false;
+let education = false
+
+if (age >=60 && education == "Matric" ) {
+    s_citizen = true;
+    y_citizen = false;
+    education = false;
+    console.log("you are S_Citzen but not MBbS, Rejected to get Visa");
+    
+}
+else if()
+
