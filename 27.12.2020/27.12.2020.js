9ea9b56978b0d3c6261e9fb42c8e60ea4a966a50
zahidmughal <zahisohna@gmail.com>
2020-12-31 10:03:17 -0800

    function console with required (2) parameter

--- 27.12.2020/27.12.2020.js
+++ 27.12.2020/27.12.2020.js
@@ -1,6 +1,9 @@
 function addNumber(num1, num2){
+    console.log("1st Num "+num1 )
+    console.log("2nd Num "+num2 )
     const result =num1+num2;
     return result;
 }
-let x =addNumber(6,7);
-console.log(x);
+// let x =addNumber(6,7);
+// console.log(x);
+console.log(addNumber(5,6));
