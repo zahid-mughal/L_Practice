6b85bf3c437dbb1b90be6811571a33b0491ef5a4
zahidmughal <zahisohna@gmail.com>
2020-12-31 10:06:25 -0800

    function console with required (2) parameter function console with 1 parameter+ default and 3

--- 27.12.2020/27.12.2020.js
+++ 27.12.2020/27.12.2020.js
@@ -1,4 +1,4 @@
-function addNumber(num1, num2){
+function addNumber(num1=0, num2=0){
     console.log("1st Num "+num1 )
     console.log("2nd Num "+num2 )
     const result =num1+num2;
@@ -7,3 +7,6 @@ function addNumber(num1, num2){
 // let x =addNumber(6,7);
 // console.log(x);
 console.log(addNumber(5,6));
+console.log(addNumber(2));
+console.log(addNumber());
+console.log(addNumber( 1,2,3));
