b738c8d3fcb94ae6b7718ab74944006b67d0c582
zahidmughal <zahisohna@gmail.com>
2021-01-01 08:43:11 -0800

    var create global

--- 27.12.2020/scope.js
+++ 27.12.2020/scope.js
@@ -2,9 +2,14 @@ let myName;
 function setVal(name){
  myName = name;
 }
-function getVal(){
-return myName;
+function check(){
+    if(true){
+    var note = "var keyword create global variable, also in function";
 }
+console.log(note);
+}
+
 setVal("zahid");
 console.log("My First Name is "+myName);
 console.log("My First Name is ",myName);
+check();
