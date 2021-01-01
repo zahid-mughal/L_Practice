ebc412d9075062b278068770906211f385de87e4
zahidmughal <zahisohna@gmail.com>
2021-01-01 08:14:28 -0800

    use tempelate literals to show greet

--- 27.12.2020/tempelte_literal.js
+++ 27.12.2020/tempelte_literal.js
@@ -1,5 +1,7 @@
 let fName = "Muhammad";
 let lName = "Zahid";
 let greet = "Hello "+fName +" "+ lName;
+let greet2 = `Hello ${fName} ${lName}`;
 
 console.log("Greeting one ",greet);
+console.log("Greeting two ",greet2);
