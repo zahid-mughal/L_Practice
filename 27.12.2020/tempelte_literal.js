38dd7ce6a1ce0fcbcf2f7f2f999a47972c632cb5
zahidmughal <zahisohna@gmail.com>
2021-01-01 08:22:17 -0800

    tempelate literals use in function for to return

--- 27.12.2020/tempelte_literal.js
+++ 27.12.2020/tempelte_literal.js
@@ -5,3 +5,7 @@ let greet2 = `Hello ${fName} ${lName}`;
 
 console.log("Greeting one ",greet);
 console.log("Greeting two ",greet2);
+function fullName(fName,lName){
+ return `${fName} ${lName}`;
+}
+console.log(fullName("Muhammad","zahid"));
